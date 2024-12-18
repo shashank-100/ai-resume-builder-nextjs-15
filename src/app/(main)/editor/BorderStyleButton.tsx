9530e97dbf1004801c71dc10e630e163475741d5
shashank-100 <shashank.telkhade@gmail.com>
2024-12-18 23:59:17 +0530

    Update BorderStyleButton.tsx

--- src/app/(main)/editor/BorderStyleButton.tsx
+++ src/app/(main)/editor/BorderStyleButton.tsx
@@ -46,7 +46,6 @@ export default function BorderStyleButton({
   return (
     <Button
       variant="outline"
-      size="icon"
       title="Change border style"
       onClick={handleClick}
     >
